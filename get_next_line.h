2928d9458f0959c1a701b9a8150f257fa3cd938b
prukngan <phongsathon.rak2003@gmail.com>
2023-07-31 21:18:58 +0700

    del#include

--- get_next_line.h
+++ get_next_line.h
@@ -12,11 +12,9 @@
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
 # include <unistd.h>
 # include <stdlib.h>
-# include <stdarg.h>
-# include <stdint.h>
-# include <stdio.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1000
